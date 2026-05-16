38dd9a524b602a82f4a7e0bfb7e626a0ae3583b9
cheenusagar <cheenusagar4@gmail.com>
2026-05-16 08:33:00 +0530

    Removed Feedback.js

--- feedback.js
+++
@@ -1,3 +0,0 @@
-let s = document.querySelectorAll('.stars span');
-s.forEach((e, i) => e.onclick = () => { s.forEach(x => x.classList.remove('active')); for (let j = 0; j <= i; j++)s[j].classList.add('active') });
-document.getElementById('f').onsubmit = e => { e.preventDefault(); alert('Thanks for feedback') };
